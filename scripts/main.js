ce9b42066a65a6e7810bbba4703c13817f153713
anti-yarek <antipin747@gmail.com>
2024-04-16 20:14:18 +0300

    loading update 2

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,7 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Display the loading screen
-    var loadingScreen = document.getElementById('loading-screen');
-    loadingScreen.style.display = 'flex'; // Show loading screen
-  
-    // Hide the loading screen once the content is fully loaded
-    window.addEventListener('load', function() {
-      loadingScreen.style.display = 'none'; // Hide loading screen
-    });
-  });
-  
+// Wait for the video to be fully loaded
+document.getElementById('loading-video').addEventListener('loadeddata', function() {
+  // Hide the loading screen
+  document.getElementById('loading-screen').style.display = 'none';
+  // Display the content
+  document.getElementById('content').style.display = 'block';
+});
