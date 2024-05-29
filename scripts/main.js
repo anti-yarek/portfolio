fe89531f1957e91c3192ea63538a51390c3dfb9b
anti-yarek <antipin747@gmail.com>
2024-05-29 21:58:08 +0300

    loading fixes

--- scripts/main.js
+++ scripts/main.js
@@ -4,20 +4,24 @@ document.addEventListener('DOMContentLoaded', function() {
     const leftCurtain = document.querySelector('.left-curtain');
     const rightCurtain = document.querySelector('.right-curtain');
 
+    let isPageLoaded = false;
     let animationLoops = 0;
 
     // Listen for the animation iteration event
     loadingAnimation.addEventListener('animationiteration', function() {
         animationLoops++;
 
-        if (animationLoops === 2) { // Adjust the number of loops as needed
+        // Check if the page has loaded and if the animation has looped at least once
+        if (isPageLoaded && animationLoops >= 1) {
             // Hide the arrows animation
             loadingAnimation.classList.add('hide-arrows');
 
-            // Make the loading screen background transparent
+            // Make the loading screen background transparent at the start of the curtain animation
             loadingScreen.style.backgroundColor = 'transparent';
 
             // Show and animate the curtains
+            leftCurtain.style.visibility = 'visible';
+            rightCurtain.style.visibility = 'visible';
             leftCurtain.classList.add('open');
             rightCurtain.classList.add('open');
 
@@ -25,10 +29,13 @@ document.addEventListener('DOMContentLoaded', function() {
             leftCurtain.addEventListener('animationend', () => {
                 loadingScreen.style.display = 'none';
             });
+
+            // Remove the animation iteration listener to prevent further calls
+            loadingAnimation.removeEventListener('animationiteration', arguments.callee);
         }
     });
 
     window.addEventListener('load', function() {
-        loadingScreen.style.display = 'flex';
+        isPageLoaded = true;
     });
 });
